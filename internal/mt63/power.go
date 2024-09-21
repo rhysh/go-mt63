f036a7f639a0d8c93d1b957d75b62eae5da21ff7
Rhys Hiltner <rhys.hiltner@gmail.com>
2024-09-20 20:26:55 -0700

    internal/mt63: calculate power in a carrier

--- internal/mt63/power.go
+++ internal/mt63/power.go
@@ -2,6 +2,7 @@ package mt63
 
 import (
 	"fmt"
+	"math"
 )
 
 type Bandwidth int
@@ -51,4 +52,27 @@ func (m *MT63) Frequency(c Carrier) float64 {
 		return float64(m.Bandwidth)/float64(64)*float64(c) + baseFreq
 	}
 	panic(fmt.Sprintf("invalid carrier %d", c))
+}
+
+type Decoder struct {
+	Mode       *MT63
+	SampleRate float64
+}
+
+func (d *Decoder) PowerAt(c Carrier, vals []float64) float64 {
+	radPerSecond := d.Mode.Frequency(c) * (2 * math.Pi)
+	var sinSum, cosSum float64
+
+	carrierCycle := 64 / float64(d.Mode.Bandwidth)
+
+	for i, val := range vals {
+		t := float64(i) / d.SampleRate
+		if t >= carrierCycle { // integer cycle count for all carriers
+			break
+		}
+		sin, cos := math.Sincos(t * radPerSecond)
+		sinSum += sin * val
+		cosSum += cos * val
+	}
+	return sinSum*sinSum + cosSum*cosSum
 }
--- internal/mt63/power_test.go
+++ internal/mt63/power_test.go
@@ -33,3 +33,83 @@ func TestModeName(t *testing.T) {
 	t.Run("", testcase(&MT63{Bandwidth: Bw500, Interleaving: Short}, "mt63-500S"))
 	t.Run("", testcase(&MT63{Bandwidth: Bw2k, Interleaving: Long}, "mt63-2kL"))
 }
+
+func TestPowerAt(t *testing.T) {
+	// Testing 500 Hz square wave "carrier" at 2000 Hz sample rate
+	mode := &MT63{Bandwidth: Bw2k, Interleaving: Long}
+	dec := &Decoder{Mode: mode, SampleRate: 2000}
+
+	square1 := []float64{ // 64 ms
+		-1, -1, 1, 1, -1, -1, 1, 1, -1, -1, 1, 1, -1, -1, 1, 1,
+		-1, -1, 1, 1, -1, -1, 1, 1, -1, -1, 1, 1, -1, -1, 1, 1,
+		-1, -1, 1, 1, -1, -1, 1, 1, -1, -1, 1, 1, -1, -1, 1, 1,
+		-1, -1, 1, 1, -1, -1, 1, 1, -1, -1, 1, 1, -1, -1, 1, 1,
+
+		-1, -1, 1, 1, -1, -1, 1, 1, -1, -1, 1, 1, -1, -1, 1, 1,
+		-1, -1, 1, 1, -1, -1, 1, 1, -1, -1, 1, 1, -1, -1, 1, 1,
+		-1, -1, 1, 1, -1, -1, 1, 1, -1, -1, 1, 1, -1, -1, 1, 1,
+		-1, -1, 1, 1, -1, -1, 1, 1, -1, -1, 1, 1, -1, -1, 1, 1,
+	}
+	square2 := []float64{ // 64 ms with phase reversal
+		-1, -1, 1, 1, -1, -1, 1, 1, -1, -1, 1, 1, -1, -1, 1, 1,
+		-1, -1, 1, 1, -1, -1, 1, 1, -1, -1, 1, 1, -1, -1, 1, 1,
+		-1, -1, 1, 1, -1, -1, 1, 1, -1, -1, 1, 1, -1, -1, 1, 1,
+		-1, -1, 1, 1, -1, -1, 1, 1, -1, -1, 1, 1, -1, -1, 1, 1,
+
+		1, 1, -1, -1, 1, 1, -1, -1, 1, 1, -1, -1, 1, 1, -1, -1,
+		1, 1, -1, -1, 1, 1, -1, -1, 1, 1, -1, -1, 1, 1, -1, -1,
+		1, 1, -1, -1, 1, 1, -1, -1, 1, 1, -1, -1, 1, 1, -1, -1,
+		1, 1, -1, -1, 1, 1, -1, -1, 1, 1, -1, -1, 1, 1, -1, -1,
+	}
+	square3 := []float64{
+		-1, 1, 1, -1, -1, 1, 1, -1, -1, 1, 1, -1, -1, 1, 1, -1,
+		-1, 1, 1, -1, -1, 1, 1, -1, -1, 1, 1, -1, -1, 1, 1, -1,
+		-1, 1, 1, -1, -1, 1, 1, -1, -1, 1, 1, -1, -1, 1, 1, -1,
+		-1, 1, 1, -1, -1, 1, 1, -1, -1, 1, 1, -1, -1, 1, 1, -1,
+	}
+	square4 := []float64{
+		1, -1, -1, 1, 1, -1, -1, 1, 1, -1, -1, 1, 1, -1, -1, 1,
+		1, -1, -1, 1, 1, -1, -1, 1, 1, -1, -1, 1, 1, -1, -1, 1,
+		1, -1, -1, 1, 1, -1, -1, 1, 1, -1, -1, 1, 1, -1, -1, 1,
+		1, -1, -1, 1, 1, -1, -1, 1, 1, -1, -1, 1, 1, -1, -1, 1,
+	}
+
+	testLarge := func(wave []float64) func(t *testing.T) {
+		return func(t *testing.T) {
+			have := dec.PowerAt(0, wave)
+			want := float64(64 * 64 / 2)
+			if have < 0.9*want || have > 1.1*want {
+				t.Errorf("want large magnitude; %f != %f", have, want)
+			}
+		}
+	}
+
+	testSmall := func(wave []float64) func(t *testing.T) {
+		return func(t *testing.T) {
+			have := dec.PowerAt(0, wave)
+			high := float64(64 * 64 / 2)
+			if have > 0.01*high {
+				t.Errorf("want small magnitude; %f != %f", have, 0.0)
+			}
+		}
+	}
+
+	// We expect PowerAt to look at only a 32 ms window of data (for the 2 kHz
+	// variant). We expect it to be indifferent to the phase of the signal.
+	//
+	// But, we expect it to detect very low power when it's centered on a phase
+	// reversal.
+
+	t.Run("double-length square wave", testLarge(square1))
+	t.Run("square head", testLarge(square1[:64]))
+	t.Run("square tail", testLarge(square1[64:]))
+	t.Run("square middle", testLarge(square1[32:]))
+
+	t.Run("double-length phase reversal", testLarge(square2))
+	t.Run("phase reversal head", testLarge(square2[:64]))
+	t.Run("phase reversal tail", testLarge(square2[64:]))
+	t.Run("phase reversal", testSmall(square2[32:]))
+
+	t.Run("90° shift", testLarge(square3))
+	t.Run("270° shift", testLarge(square4))
+}
